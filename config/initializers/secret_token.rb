8d8e6fd23bde466e5c088b495111a8c66c5e8954
Michael Pope <amorphid@captaincoder.io>
2014-01-27 11:47:00 -0800

    Create Rails app called butter_knife
    [ticket: 64404658]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ButterKnife::Application.config.secret_key_base = 'e756edf9b1fba7a0ed13f261e002625396abd68f50bd04110a19ea0e297365286fe94153e2aef8967c1b51668184d579c95d5633650ec36ae0db678b6404621a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ButterKnife::Application.config.session_store :cookie_store, key: '_butter_knife_session'
